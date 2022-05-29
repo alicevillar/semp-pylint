f77ce563af5973babfc0aa358ca467b464b17d82
Alice Villar <alsaldanhavillar@gmail.com>
2022-05-29 16:18:37 -0300

    final version - 10.00

--- code_with_lint.py
+++ code_with_lint.py
@@ -1,31 +1,24 @@
+""" Pylint Study"""
 
 SOME_GLOBAL_VAR = 'GLOBAL VAR NAMES SHOULD BE IN ALL_CAPS_WITH_UNDERSCOES'
 
+
 def multiply(variable_x, variable_y):
-    result = variable_x* variable_y
+    """ This function aims to multiply two numbers. """
+    result = variable_x * variable_y
     if result == 777:
         print("jackpot!")
     return result
+
 
 def is_sum_lucky(variable_x, variable_y):
     """This returns a string describing whether or not the sum of input is lucky
     This function first makes sure the inputs are valid and then calculates the
     sum. Then, it will determine a message to return based on whether or not
-    that sum should be considered "lucky"
-    """
+    that sum should be considered lucky"""
     if variable_x is not None:
         if variable_y is not None:
             result = variable_x+variable_y
             if result == 7:
                 return 'a lucky number!'
-
-        return 'just a normal number'
-
-
-class SomeClass():
-    """"
-    Classe para xxxx
-    """
-    def __init__(self, some_arg,  some_other_arg):
-        self.some_other_arg = some_other_arg
-        self.some_arg = some_arg
+    return 'just a normal number'
